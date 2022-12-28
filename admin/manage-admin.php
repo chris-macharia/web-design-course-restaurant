17f8ab33f0e5452654ac021c5c5346a3aa70e631
chris-macharia <christophermaina003@gmail.com>
2022-12-28 13:25:42 +0300

    Add a dashboard to the index.php file and also Add a manage-admin.php file

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -29,34 +29,7 @@
   <!-- Main content section starts -->
   <div class="main-content">
     <div class="wrapper">
-      <h1> DASHBOARD</h1>
-
-      <div class="col-4 text-center">
-        <h1>5</h1>
-        <br />
-        categories
-      </div>
-
-      <div class="col-4 text-center">
-        <h1>5</h1>
-        <br />
-        categories
-      </div>
-
-      <div class="col-4 text-center">
-        <h1>5</h1>
-        <br />
-        categories
-      </div>
-
-      <div class="col-4 text-center">
-        <h1>5</h1>
-        <br />
-        categories
-      </div>
-
-      <div class="clear-fix"></div>
-
+      <h1> MANAGE ADMIN</h1>
     </div>
   </div>
   <!-- Main section ends -->
