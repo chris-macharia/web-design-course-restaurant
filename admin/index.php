d4610fc9f286673503b80fda1510e59695957727
chris-macharia <christophermaina003@gmail.com>
2022-12-28 12:50:44 +0300

    Create a template for the Admin panel

---
+++ admin/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Food order website - Home Page</title>
+
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+  <h1 class="text-center">Admin panel</h1>
+  
+  <!-- Menu section starts hear -->
+  <div class="menu text-center">
+    <div class="wrapper">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Admin</a></li>
+        <li><a href="#">Category</a></li>
+        <li><a href="#">Food</a></li>
+        <li><a href="#">Orders</a></li>
+      </ul>
+    </div>
+  </div>
+  <!-- Menu section ends hear -->
+
+  <!-- Main content section starts -->
+  <div class="main-content">
+    <div class="wrapper">
+      main-content goes here
+    </div>
+  </div>
+  <!-- Main section ends -->
+
+  <!-- Footer section starts -->
+  <div class="footer">
+    <div class="wrapper">
+      <p class="text-center">
+        2022 All rights  deserved. Designed by - <a href="#">Chris Macharia</a>
+      </p>
+    </div>
+  </div>
+  <!-- Footer section ends -->
+</body>
+
+</html>
