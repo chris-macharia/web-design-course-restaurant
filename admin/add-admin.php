ba16352bd4127f780914fd121fcb719e0177883c
chris-macharia <christophermaina003@gmail.com>
2023-01-02 18:37:39 +0300

    Fetch data from 'add-admin-page' and save it in the DB

--- admin/add-admin.php
+++ admin/add-admin.php
@@ -43,3 +43,30 @@
 </div>
 
 <?php include("./partials/footer.php"); ?>
+
+<?php
+  //Process the value from Form and save it in DB
+
+  // Check wherther the submit button is clicked
+      if(isset($_POST['submit']))
+  {
+      // Button clicked
+      $full_name = $_POST['full_name'];
+      $username = $_POST['username'];
+      $password = md5($_POST['password']);  //Password encryption with MD5
+
+      
+      //SQL querry to save the data into the database
+      $sql = "INSERT INTO tbl_admin SET
+        full_name='$full_name',
+        username='$username',
+        password='$password'
+      ";
+
+      // Execute query and save data in db
+      $conn = mysqli_connect('localhost', 'root', '') or die(mysqli_error()); //create db connection
+      $db_select = mysqli_select_db($conn, 'food-ordering-website') or die(mysqli_error()); //select DB 
+
+      $res = mysqli_query($conn, $sql) or die(mysqli_error());
+  }
+?>
