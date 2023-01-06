fa458a189bd92ed4dacfe5e92fdc3706345b89a0
chris-macharia <christophermaina003@gmail.com>
2023-01-06 21:53:04 +0300

    Create a form to change the password

--- admin/update-password.php
+++ admin/update-password.php
@@ -5,7 +5,45 @@
       <h1>Change Password</h1>
       <br><br>
 
-      
+      <form action="" method="POST">
+        <table class="tbl-30">
+          <tr>
+            <td>
+              Current Password
+            </td>
+            <td>
+              <input type="password" name="old_password" placeholder="Old Password">
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              New Password:
+            </td>
+            <td>
+              <input type="password" name="new_password" placeholder="New Password">
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              Confirm Password
+            </td>
+            <td>
+              <input type="password" name="confirm_password" placeholder="confirm password">
+            </td>
+          </tr>
+
+          <tr>
+            <td colspan="2">
+              <input type="submit" name="submit" value="Change Password">
+
+            </td>
+          </tr>
+
+
+        </table>
+      </form>
     </div>
   </div>
 
