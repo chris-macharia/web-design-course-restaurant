892fd1d59efadacb284dec9e5ffa2ba22e9ccb85
chris-macharia <christophermaina003@gmail.com>
2023-01-24 18:42:03 +0300

    Add a Login page

---
+++ admin/login.php
@@ -0,0 +1,35 @@
+<html>
+
+<head>
+  <title>Login - Food Order System</title>
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+
+  <div class="login">
+    <h1 class="text-center">Login</h1>
+    <br>
+    <br>
+
+    <!-- login form starts here -->
+    <form action="" method="POST" class="text-center">
+      Username: <br>
+      <input type="text" name="username" placeholder="Enter User Name"> <br><br>
+      Password: <br>
+      <input type="text" name="password" placeholder="Enter User Password"> <br>
+      <br>
+      <br>
+
+      <input type="submit" name="submit" value="Login" class="btn-primary">
+
+    </form>
+    <br>
+    <!-- login form ends here -->
+
+    <p class="text-center">Created by <a href="#">Chris Macharia</a> </p>
+  </div>
+
+</body>
+
+</html>
