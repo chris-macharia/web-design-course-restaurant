5338778697d6c2fed32265ef7723a514c9b8c8c4
chris-macharia <christophermaina003@gmail.com>
2023-02-09 06:13:53 +0300

    Collect all user data from the 'add-food' form

--- admin/add-food.php
+++ admin/add-food.php
@@ -6,6 +6,13 @@
 
     <br><br>
 
+    <?php
+    if (isset($_SESSION['upload'])) {
+      echo $_SESSION['upload'];
+      unset($_SESSION['upload']);
+    }
+    ?>
+
     <form action="" method="POST" enctype="multipart/form-data">
 
       <table class="tbl-30">
@@ -99,6 +106,59 @@
       </table>
 
     </form>
+    <?php
+    if (isset($_POST['submit'])) {
+
+      //Add the food in the DB
+      //1.Get the data from the DB
+      $title = $_POST['title'];
+      $description = $_POST['description'];
+      $price = $_POST['price'];
+      $category = $_POST['category'];
+
+      //Radio buttons
+      if (isset($_POST['featured'])) {
+        $featured = $_POST['featured'];
+      } else {
+        $featured = "No"; //Setting the default value
+      }
+
+      if (isset($_POST['active'])) {
+        $active = $_POST['active'];
+      } else {
+        $active = "No";
+      }
+
+      //2.Upload the image if selected
+      //Upload the image only if the image was selected !!WATCH OUT, SOME USERS MAY CLICK THE BUTTON BUT MAYBE CHANGE THEIR MINDS.
+      if (isset($_FILES['image']['name'])) {
+        $image_name = $_FILES['image']['name'];
+
+        if ($image_name != "") //WATCH OUT FOR THIS: only if image is selected
+        {
+          //Rename the image
+          $ext = end(explode('.', $image_name));
+
+          $image_name = "Food-name-" . rand(0000, 9999) . "." . $ext;
+
+          //Upload the image
+          $src = $_FILES['image']['tmp_name'];
+
+          $dest = "../images/food/" . $image_name;
+
+          $upload = move_uploaded_file($src, $dest);
+
+          if ($upload == false) {
+            $_SESSION['upload'] = "<div class='error'>Failed to upload Image.</div>";
+            header('location:' . HOMEURL . 'admin/add-food.php');
+            die();
+          }
+        }
+      } else {
+        $image_name = "";
+      }
+    }
+    ?>
   </div>
 </div>
 
